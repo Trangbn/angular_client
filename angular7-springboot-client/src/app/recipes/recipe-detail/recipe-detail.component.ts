d001b8962afbbc17e1ec9b7947218954914b67fa
TrangBN <trangbn580@gmail.com>
2019-06-01 13:34:05 +0700

    commit change PMTCTChildInf

--- angular7-springboot-client/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ angular7-springboot-client/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,5 +12,4 @@ export class RecipeDetailComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
